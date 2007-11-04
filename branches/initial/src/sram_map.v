721a504d1dfbb1c436cbee6bbb0a8e17af1c5ce9
nobody <nobody@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2007-11-04 17:46:24 +0000

    This commit was manufactured by cvs2svn to create branch 'initial'.

---
+++ branches/initial/src/sram_map.v
@@ -0,0 +1,24 @@
+module sram_map(SRAM_ADDR, SRAM_DQ, SRAM_WE_N, SRAM_UB_N, SRAM_LB_N, memwr_n, abus, dout, din);
+output [14:0] 	SRAM_ADDR;
+inout  [15:0] 	SRAM_DQ;
+output 			SRAM_WE_N;
+output 			SRAM_UB_N;
+output 			SRAM_LB_N;
+input			memwr_n;
+input  [15:0]	abus;
+input  [7:0]	dout;
+output [7:0]	din;
+
+assign SRAM_ADDR = abus[15:1];
+assign SRAM_UB_N = ~abus[0];
+assign SRAM_LB_N = abus[0];
+assign SRAM_WE_N = memwr_n;
+
+wire [7:0] effective_do = memwr_n ? 8'bZZZZZZZZ : dout;
+
+assign SRAM_DQ[7:0]  = abus[0] ? 8'bZZZZZZZZ : effective_do;
+assign SRAM_DQ[15:8] = abus[0] ? effective_do : 8'bZZZZZZZZ;
+
+assign din = abus[0] ? SRAM_DQ[15:8] : SRAM_DQ[7:0];
+
+endmodule
---
+++ branches/initial/vector06cc.cdf
@@ -0,0 +1,13 @@
+/* Quartus II Version 7.1 Build 156 04/30/2007 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP2C20F484) Path("C:/altera/71/qdesigns/vector06cc/") File("vector06cc.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
