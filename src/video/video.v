76474d2e02393b131c50b5a71e10e64880d1aee8
svo <svo@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2007-11-29 00:58:20 +0000

    new timings: waitstates chosen according to the original Vector device timings fixed for m@color and dizzyrek2, basic korvet works too proper scan doubler allows for realtime effects text on border works
    awesomeness!
    
    (also T80 fixed to have proper timings)

--- src/video/video.v
+++ src/video/video.v
@@ -3,6 +3,7 @@
 module video(
 	clk24, 		// latched clocks
 	ce12,		// 12mhz
+	ce6,
 	video_slice,
 	pipe_ab,
  
@@ -15,19 +16,22 @@ module video(
 	vsync, 
 	videoActive, 
 	coloridx,
-	border,
+	realcolor_in,
+	realcolor_out,
 	retrace,
     video_scroll_reg,
+	border_idx,
 	testpin
 );
 
 parameter SCREENWIDTH = 10'd640;	// constants, don't even try using other values
 //parameter SCREENHEIGHT = 10'd600;
-parameter SCREENHEIGHT = 10'd584;//10'd576;
+parameter SCREENHEIGHT = 10'd584;
 
 // input clocks
 input 			clk24;
 input 			ce12;
+input			ce6;
 input 			video_slice;
 input 			pipe_ab;
 
@@ -42,13 +46,16 @@ output 			hsync;
 output 			vsync;
 output 			videoActive;
 output [3:0] 	coloridx;
-output 			border;
+input  [7:0]	realcolor_in;
+output [7:0]	realcolor_out;
+//output 			border;
 output 			retrace;
 
 input  [7:0]	video_scroll_reg;
+input  [3:0]	border_idx;
 
 // test pins
-output [1:0] 	testpin;
+output [3:0] 	testpin;
 
 reg[9:0] realx;  
 reg[9:0] realy;
@@ -60,12 +67,14 @@ reg[9:0] scanyy;			// y-state timer/counter
 
 reg bordery;
 wire borderx;
-assign border = bordery | borderx;
+wire border;
 
 reg videoActiveX;			// 1 == X is within visible area
 reg videoActiveY;			// 1 == Y is within visible area
 wire videoActive = videoActiveX & videoActiveY;
-wire retrace = !videoActiveY;
+
+assign retrace = !videoActiveY;
+
 reg pre_xstart, pre_xend;
 
 parameter state0 = 3'b000, state1 = 3'b001, state2 = 3'b010, state3 = 3'b011, state4 = 3'b100;
@@ -73,9 +82,7 @@ parameter state0 = 3'b000, state1 = 3'b001, state2 = 3'b010, state3 = 3'b011, st
 assign hsync = !(scanxx_state == state2);
 assign vsync = !(scanyy_state == state2);
 
-
 reg scanyy_minus;			// todo: investigate if this is still necessary
-
 
 //
 // framebuffer variables
@@ -90,7 +97,11 @@ always @(posedge clk24) begin
 			case (scanyy_state)
 			state0:
 					begin
-						scanyy <= 1 + 24 - 8 + 24 - 14;
+`ifdef x800x600					
+						scanyy <= 27;
+`else
+						scanyy <= 17 - 0;
+`endif						
 						scanyy_state <= state1;
 						videoActiveY <= 0;
 					end
@@ -101,12 +112,16 @@ always @(posedge clk24) begin
 					end
 			state2: // BACK PORCH + TOP BORDER
 					begin
-						scanyy <= 19;
+						scanyy <= 19 - 4;
 						scanyy_state <= state3;
 					end
 			state3:
 					begin
+`ifdef x800x600					
 						scanyy <= SCREENHEIGHT + 14;
+`else
+						scanyy <= SCREENHEIGHT + 4;
+`endif						
 						scanyy_state <= state0;
 						videoActiveY <= 1;
 						realy <= 0;
@@ -133,11 +148,22 @@ always @(posedge clk24) begin
 						scanyy_minus <= 1'b1;
 						videoActiveX <= 1'b0;
 						realy <= realy + 1'b1;
+				
 						fb_row <= fb_row - 1'b1;
 						if (fb_row_count != 0) begin
 							fb_row_count <= fb_row_count - 1'b1;
 						end 
 							else bordery <= 1;
+							
+						if (realy == 42) begin
+							fb_row <= {video_scroll_reg, 1'b1};
+							fb_row_count <= 511;
+							bordery <= 0;
+						end else if (realy == 0) begin
+							fb_row <= 1;
+						end
+						
+							
 					end
 			state1: // enter HSYNC PULSE
 					begin 
@@ -147,7 +173,11 @@ always @(posedge clk24) begin
 					end
 			state2:	// enter BACK PORCH + RIGHT BORDER
 					begin
+`ifdef x800x600					
 						scanxx <= 10'd61 - 1'b1 - 32;
+`else
+						scanxx <= 10'd61 - 1'b1;
+`endif						
 						scanxx_state <= state3;
 					end
 			state3:	// enter VISIBLE AREA
@@ -179,27 +209,85 @@ always @(posedge clk24) begin
 			realx <= realx + 1'b1;
 		end
 		
-		if (realy == 32) begin
-			fb_row <= {video_scroll_reg, 1'b1};
-			fb_row_count <= 511;
-			bordery <= 0;
-		end
 end
 
 wire [3:0] coloridx_modeless;
-framebuffer winrar(clk24, ce12, video_slice, pipe_ab, fb_row[8:1], hsync, SRAM_DQ, SRAM_ADDR, coloridx_modeless, borderx, testpin);
 
-reg [3:0] coloridx;
+assign border = bordery | borderx;
+
+framebuffer winrar(clk24, 
+`ifdef DOUBLE_BUFFER
+	ce6,
+`else
+	ce12, 
+`endif
+	video_slice, pipe_ab, fb_row[8:0], hsync, SRAM_DQ, SRAM_ADDR, coloridx_modeless, borderx/*, testpin*/);
+
+reg [3:0] xcoloridx;
+
 always @(coloridx_modeless or clk24) begin
 	if (mode512) begin
-		if (ce12)
-			coloridx <= {2'b00, coloridx_modeless[2], coloridx_modeless[3]};
+		if (
+`ifdef DOUBLE_BUFFER
+		ce6
+`else		
+		ce12
+`endif		
+		)
+			xcoloridx <= {2'b00, coloridx_modeless[2], coloridx_modeless[3]};
 		else
-			coloridx <= {2'b00, coloridx_modeless[1], coloridx_modeless[0]};
+			xcoloridx <= {2'b00, coloridx_modeless[1], coloridx_modeless[0]};
 	end
 	else
-		coloridx <= coloridx_modeless;
+		xcoloridx <= coloridx_modeless;
 end
+
+`ifdef DOUBLE_BUFFER
+
+wire [7:0] rc_a;
+wire [7:0] rc_b;
+
+wire wren_line1 = fb_row[1];
+wire wren_line2 = ~fb_row[1];
+
+reg reset_line;
+
+always @(posedge clk24) begin
+	reset_line <= fb_row[0] & !hsync;
+end
+
+assign testpin = {reset_line, wren_line1, reset_line, mode512};
+
+rambuffer #(4)line1(.clk(clk24),
+				.cerd(1),
+				.cewr(ce12),
+				.wren(wren_line1),
+				.resetrd(!hsync),
+				.resetwr(reset_line),
+				.din(realcolor_in),
+				.dout(rc_a)
+				);
+				
+rambuffer #(8)line2(.clk(clk24),
+				.cerd(1),
+				.cewr(ce12),
+				.wren(wren_line2),
+				.resetrd(!hsync),
+				.resetwr(reset_line),
+				.din(realcolor_in),
+				.dout(rc_b)
+				);
+				
+//assign coloridx = wren_line1 ? ci_b : ci_a;
+assign coloridx = border ? border_idx : xcoloridx;
+//assign coloridx = border ? border_idx : border_idx;
+assign realcolor_out = wren_line1 ? rc_b : rc_a;
+`else
+
+assign coloridx = xcoloridx;
+
+`endif
+
 	
 endmodule
 
