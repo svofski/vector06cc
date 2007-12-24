24352e33375db2a8c8c1904d193c14da335d6194
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2007-12-24 19:59:47 +0000

    mmc reads first sector, hooray!

--- trunk/src/floppy/spi.v
+++ trunk/src/floppy/spi.v
@@ -6,7 +6,7 @@ input 		ce;
 input		reset_n;
 output	reg	mosi;
 input		miso;
-output		sck = ~clk;
+output		sck = ~clk & scken;
 
 input [7:0]	di;
 input		wr;
@@ -18,12 +18,14 @@ reg [7:0]	shiftreg;
 reg [7:0]	shiftski;
 
 reg [1:0]	state = 0;
+reg 		scken = 0;
 
 always @(posedge clk or negedge reset_n) begin
 	if (!reset_n) begin
 		state <= 0;
-		mosi <= 1'bZ;
+		mosi <= 1'b0;
 		dsr  <= 0;
+		scken <= 0;
 	end else begin
 		if (ce) begin
 			case (state)
@@ -36,16 +38,21 @@ always @(posedge clk or negedge reset_n) begin
 					end
 				end
 			1: 	begin
-					mosi <= shiftreg[0];
-					shiftreg <= {miso,shiftreg[7:1]};
+					scken <= 1;
+					mosi <= shiftreg[7];
+					shiftreg <= {shiftreg[6:0],miso};
 					shiftski <= {1'b0,shiftski[7:1]};
 					
-					if (|shiftski == 0) state <= 2;
+					if (|shiftski == 0) begin 
+						state <= 2;
+						scken <= 0;
+					end
 				end
 			2:	begin
-					mosi <= 1'bZ;
+					mosi <= 1'b0; // shouldn't be necessary but a nice debug view
 					dsr <= 1'b1;
 					state <= 0;
+					//scken <= 0;
 				end
 			default: ;
 			endcase
