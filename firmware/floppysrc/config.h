7a833f063e5d4dfb195357a37c898b95a56327e9
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2008-01-05 20:46:00 +0000

    first draft of cpu slave software

---
+++ firmware/floppysrc/config.h
@@ -0,0 +1,7 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define SECTOR_SIZE 		512
+#define SECTOR_SIZE_CODE	2		// 0 = 128, 1 = 256, 2 = 512, 3 = 1024
+
+#endif
---
+++ firmware/floppysrc/slave.h
@@ -0,0 +1,21 @@
+#ifndef _SLAVE_H
+#define _SLAVE_H
+
+#include "integer.h"
+
+#define SR_OPENERR	1
+#define	SR_READERR	2
+#define SR_FORMAT	3
+
+#define CPU_REQUEST_READ 	0x10 	// Request to read one sector at oTRACK, oSECTOR, bit 0 is side (head #) 
+#define CPU_REQUEST_WRITE	0x20 	// Request to write one sector at oTRACK, oSECTOR; bit 0 is side (head #) 
+#define CPU_REQUEST_READADDR 0x30	// Request to read sector address (return 6 byte header) 
+#define CPU_REQUEST_ACK		0x80	// clear status
+
+#define CPU_STATUS_COMPLETE	0x01
+#define CPU_STATUS_ERROR	0x02
+
+
+uint8_t slave(const char *imagefile, uint8_t *buffer);
+
+#endif
