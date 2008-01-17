e94a75e4ffd49a4c9fcfa59c9ba4dd26f9d1bf38
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2008-01-17 00:52:48 +0000

    kvaz: changed to selectors instead of priority encoder, hoping for more stable operation 8253: changed all to posedge; skip one tce after counter load (seems to be more like the original)       the test (skynet) passes, but i'm not sure if latched read works for two bytes.. almost sure it doesn't
    firmware: sort of "automount" implemented, now one can remove sd card, insert it with another file and it would 
    be as if a disk was replaced; although one bdos error has to happen for remount attempt to be made

--- trunk/src/i8253/8253.v
+++ trunk/src/i8253/8253.v
@@ -23,7 +23,7 @@
 
 `default_nettype none
 
-module pit8253(clk, ce, tce, a, wr, rd, din, dout, gate, out, testpin, tpsel);
+module pit8253(clk, ce, tce, a, wr, rd, din, dout, gate, out, testpin, tpsel, intq0, counter_load);
 	input 			clk;		// i: i/o clock
 	input 			ce;			// i: i/o clock enable
 	input 			tce;		// i: timer clock enable, one for all 3 timers
@@ -31,12 +31,14 @@ module pit8253(clk, ce, tce, a, wr, rd, din, dout, gate, out, testpin, tpsel);
 	input 			wr;			// i: data write
 	input 			rd;			// i: data read
 	input [7:0] 	din;		// i: data input bus
-	output[7:0] 	dout;		// o: data output bus
+	output reg[7:0] dout;		// o: data output bus
 	input [2:0] 	gate;		// i: gate inputs, NOT USED
 	output [2:0] 	out;		// o: timer outputs
 	output [9:0] 	testpin;	// o: test pins
 	input 			tpsel;		// i: test pin group selector
-
+	output [15:0]	intq0;
+	output [15:0] 	counter_load;
+	
 wire	[7:0] 	q0;
 wire	[7:0]	q1;
 wire	[7:0]	q2;
@@ -45,34 +47,21 @@ reg		[5:0]	cword0;
 reg		[5:0]	cword1;
 reg		[5:0]	cword2;
 
-reg [3:0] wren;
-reg [3:0] rden;
-reg [2:0] cwsel;
+reg 	[2:0] cwsel;
+reg 	[3:0] sel;
+wire 	[3:0] wren;
+wire	[3:0] rden;
 
-always @(wr,a) begin
-	if (wr) 
-		case (a)
-		2'b00:	wren <= 4'b0001;
-		2'b01:	wren <= 4'b0010;
-		2'b10:	wren <= 4'b0100;
-		2'b11:	wren <= 4'b1000;	// cw
-		endcase
-	else
-		wren <= 4'b0000;
-end
+always @(a) 
+	case(a)
+		2'b00:	sel <= 4'b0001;
+		2'b01:	sel <= 4'b0010;
+		2'b10:	sel <= 4'b0100;
+		2'b11:	sel <= 4'b1000;
+	endcase
 
-always @(rd,a) begin
-	if (rd) 
-		case (a)
-		2'b00:	rden <= 4'b0001;
-		2'b01:	rden <= 4'b0010;
-		2'b10:	rden <= 4'b0100;
-		2'b11:	rden <= 4'b1000;	// cw
-		endcase
-	else
-		rden <= 4'b0000;
-end
-
+assign wren = {4{wr}} & sel;
+assign rden = {4{rd}} & sel;
 
 always @(din) begin
 	case (din[7:6]) 
@@ -87,15 +76,22 @@ wire [9:0] testpin0;
 wire [9:0] testpin1;
 assign testpin = tpsel ? testpin0 : testpin1;
 
-assign dout = rden[0] ? q0 : rden[1] ? q1 : rden[2] ? q2 : 0;
+//assign dout = rden[0] ? q0 : rden[1] ? q1 : rden[2] ? q2 : 0;
+always @(rden) 
+	case (rden) 
+		3'b001:	dout <= q0;
+		3'b010:	dout <= q1;
+		3'b100:	dout <= q2;
+		default:dout <= 0;
+	endcase
 
-pit8253_counterunit cu0(clk, ce, tce, din, wren[3] & cwsel[0], din, wren[0], rden[0], q0, gate[0], out[0], testpin0);
+pit8253_counterunit cu0(clk, ce, tce, din, wren[3] & cwsel[0], din, wren[0], rden[0], q0, gate[0], out[0], testpin0, intq0, counter_load);
 pit8253_counterunit cu1(clk, ce, tce, din, wren[3] & cwsel[1], din, wren[1], rden[1], q1, gate[1], out[1]);
 pit8253_counterunit cu2(clk, ce, tce, din, wren[3] & cwsel[2], din, wren[2], rden[2], q2, gate[2], out[2], testpin1);
 
 endmodule
 
-module pit8253_counterunit(clk, ce, tce, cword, cwset, d, wren, rden, dout, gate, out, testpins);
+module pit8253_counterunit(clk, ce, tce, cword, cwset, d, wren, rden, dout, gate, out, testpins, value, counter_load);
 	input	clk;			// whatever main clk
 	input	ce;				// bus clock enable, e.g. 3MHz
 	input	tce;			// timer clock enable, e.g. 1.5MHz
@@ -109,8 +105,10 @@ module pit8253_counterunit(clk, ce, tce, cword, cwset, d, wren, rden, dout, gate
 	output	out;			// out pin according to mode
 
 	output [9:0] testpins;
-
-assign testpins = {read_msb, counter_loaded, counter_loading, rl_mode, cw_mode};
+	output [15:0] value = counter_q;
+	output [15:0] counter_load;
+	
+assign testpins = {counter_wren, read_msb, counter_loaded, counter_starting, rl_mode, cw_mode};
 
 parameter M0 = 3'd0, M1 = 3'd1, M2 = 3'd2, M3 = 3'd3, M4 = 3'd4, M5 = 3'd5;
 
@@ -139,7 +137,7 @@ reg [15:0] 	counter_load;
 reg 		counter_wren;
 wire[15:0] 	counter_q;
 
-reg [15:0]	read_latch; 		// latched value
+reg [7:0]	read_latch; 		// latched value
 reg [1:0]	read_msb;			// double-byte read mode
 
 
@@ -150,6 +148,7 @@ wire counter_clock_enable = tce & /*counter_ena &*/ counter_loaded & !loading_st
 
 reg			loading_msb;	// for rl=3: 0: next 8-bit value will be lsb, 1: msb
 
+reg counter_starting;
 reg counter_loaded;
 reg counter_loading;
 
@@ -160,8 +159,8 @@ always @(posedge clk) begin
 	if (cwset) begin
 		if (cword[5:4] == 2'b00) begin
 			read_msb <= 3;
-			read_latch <= counter_q;
-			dout <= 8'b0;
+			read_latch <= counter_q[15:8];
+			dout <= counter_q[7:0];
 		end else begin
 			loading_msb <= 0;	// reset the doorstopper
 			counter_loaded <= 0;
@@ -207,18 +206,21 @@ always @(posedge clk) begin
 		case (rl_mode)
 		2'b01:	begin
 					counter_load[7:0] <= d;
-					counter_loaded <= 1;
+					//counter_loaded <= 1;
+					counter_starting <= 1;
 					counter_wren <= 1;
 				end
 		2'b10: 	begin
 					counter_load[15:8] <= d;
-					counter_loaded <= 1;
+					//counter_loaded <= 1;
+					counter_starting <= 1;
 					counter_wren <= 1;
 				end
 		2'b11:	begin
 					if (loading_msb) begin
 						counter_load[15:8] <= d;
-						counter_loaded <= 1;
+						//counter_loaded <= 1;
+						counter_starting <= 1;
 						counter_loading <= 0;
 						counter_wren <= 1;
 					end	else begin
@@ -237,11 +239,13 @@ always @(posedge clk) begin
 	if (rden & ce) begin
 		case (read_msb) 
 			3: 	begin
-					dout <= read_latch[7:0];
-					read_msb <= 2;
+					//dout <= read_latch[7:0];
+					dout <= read_latch;
+					read_msb <= 0;
 				end
 			2: 	begin
-					dout <= read_latch[15:8];
+					//dout <= read_latch[15:8];
+					//dout <= read_latch;
 					read_msb <= 0;
 				end
 			1: 	begin
@@ -268,6 +272,12 @@ always @(posedge clk) begin
 	// reset counter_wren on next tce
 	if (tce & counter_wren) begin
 		counter_wren <= 0;
+	end
+
+	// enable counting on next tce
+	if (tce & counter_starting) begin
+		counter_loaded <= 1;
+		counter_starting <= 0;
 	end
 	
 	if (tce) begin
@@ -325,13 +335,11 @@ wire [15:0] next = counter - (~halfmode ? 16'd1 : counter[0] == 1'b0 ? 16'd2 : o
 
 assign q = counter;
 
-always @(negedge clk) begin
-	if (ce) begin
-		if (wren) 
-			counter <= d;
-		else
-			counter <= next;
-	end
+always @(posedge clk) begin
+	if (wren) 
+		counter <= d;
+	else 
+		if (ce) counter <= next;
 end
 endmodule
 
