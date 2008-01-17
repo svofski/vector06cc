5bb6b5f1da12f074199e4ae442d666544e36336a
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2008-01-17 23:47:10 +0000

    + cputype   (diagrams)

---
+++ workbench/cputest/make.bat
@@ -0,0 +1,3 @@
+@call build.bat test
+@call build.bat cputype
+@call build.bat timer
--- workbench/cputest/timer.bat
+++
@@ -1,5 +0,0 @@
-del timer.com
-c:\bin\as\bin\asw timer.asm
-c:\bin\as\bin\p2bin -r 256-2048 timer.p
-ren timer.bin timer.com
-del timer.p
