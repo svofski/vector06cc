271dddda2254b437db1a35ad3a7d728049869e59
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2008-01-26 03:16:10 +0000

    comment headers

--- trunk/firmware/floppysrc/philes.h
+++ trunk/firmware/floppysrc/philes.h
@@ -1,3 +1,22 @@
+// ====================================================================
+//                         VECTOR-06C FPGA REPLICA
+//
+//                  Copyright (C) 2007,2008 Viacheslav Slavinsky
+//
+// This code is distributed under modified BSD license. 
+// For complete licensing information see LICENSE.TXT.
+// -------------------------------------------------------------------- 
+//
+// An open implementation of Vector-06C home computer
+//
+// Author: Viacheslav Slavinsky, http://sensi.org/~svo
+// 
+// Source file: philes.h
+//
+// FAT FS toplevel interface
+//
+// --------------------------------------------------------------------
+
 #ifndef _PHILES_H
 #define _PHILES_H
 
