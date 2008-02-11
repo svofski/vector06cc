81b418e8b457b216b407292115873cf62b882fd6
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2008-02-11 22:25:28 +0000

    DMA for disk write ops

--- src/floppy/dma_read.v
+++ src/floppy/dma_read.v
@@ -21,9 +21,9 @@
 // While ready == 0, nothing is allowed to access data and address busses.
 // --------------------------------------------------------------------
 
-module dma_read(clk, ce, reset_n, iaddr, oaddr, odata, owren, nblocks, ready, ospi_data, ispi_data, ospi_wr, ispi_dsr, debug);
+module dma_read(clk, ce, reset_n, iaddr, oaddr, odata, idata, owren, nblocks, ready, ospi_data, ispi_data, ospi_wr, ispi_dsr, debug);
 
-parameter IDLE=0, BUSY=1, BLOCK=2, OVER=3, NBYTE=4;
+parameter IDLE=0, BUSY=1, BLOCK=2, OVER=3, NBYTE=4, MBYTE=5;
 
 input 				clk;			// clock
 input				ce;				// clock enable
@@ -32,28 +32,38 @@ input				reset_n;		// reset
 input  [15:0] 		iaddr;			// this addr latched as start of the buffer when beginning transfer
 output reg [15:0] 	oaddr;			// output address bus
 output reg[7:0]		odata;			// output data (direct from SPI controller)
+input 	[7:0]		idata;			// data from RAM 
 output reg			owren;			// RAM write strobe (positive)
 
-input	[3:0]		nblocks;		// input: this is the amount of 512-byte blocks to transfer
-									// 		  a non-zero value initiates transfer
+input	[3:0]		nblocks;		// input: bits [2:0] is the amount of 512-byte blocks to transfer
+									// 		  		      a non-zero value initiates transfer
+									//		  bit  3: 	 0: transfer from spi to host ("read")
+									//                   1: transfer from host to spi ("write")
 output				ready = !busy;	// when 0, the controller has exclusive access to busses
 
-output	[7:0]		ospi_data = 8'hFF;	// this is poked into SPI for data reception
+output	[7:0]		ospi_data; 		// this data fed to SPI 
 input	[7:0]		ispi_data;		// SPI data input, routed directly to odata output
 output	reg			ospi_wr;		// SPI transfer initiator
 input				ispi_dsr;		// SPI data ready
 output  [7:0]		debug = {rblocks,state};
 
-reg [3:0] rblocks;
+
+reg [7:0]	idata_r;
+reg [2:0] 	rblocks;
+reg			dir_tospi;				// 0: send data from spi to host ram ("read")
+									// 1: send data from host ram to spi ("write")
 
 reg [15:0]  addrbase;
-reg [9:0]  bytectr;
+reg [9:0]  	bytectr;
 
 reg [3:0] state;
 
 reg		busy;
 
 always odata <= ispi_data;
+
+// if direction is set FROM spi TO host, poke out FF's
+assign ospi_data = idata_r;	
 
 always @(posedge clk) 
 	if (~reset_n) begin
@@ -68,16 +78,22 @@ always @(posedge clk)
 		IDLE:	
 			begin
 				if (nblocks != 0) begin
-					rblocks <= nblocks;
+					rblocks <= nblocks[2:0];
+					dir_tospi <= nblocks[3];
 					busy <= 1;
-					addrbase <= iaddr;
+					addrbase <= iaddr;// - (~nblocks[3]);
 					oaddr <= iaddr;
 					bytectr <= 512;
 					state <= NBYTE;
 				end
 			end
+		MBYTE:
+			begin
+				state <= NBYTE;
+			end
 		NBYTE:
 			begin
+				idata_r <= dir_tospi ? idata : 8'hFF;
 				ospi_wr <= 1;
 				owren <= 0;
 				state <= BUSY;
@@ -86,8 +102,12 @@ always @(posedge clk)
 			begin
 				ospi_wr <= 0;
 				if (ispi_dsr) begin
-					owren <= 1'b1;
-					oaddr <= addrbase + (512 - bytectr);
+					owren <= ~dir_tospi;//1'b1;
+					// when reading: (addr=0), read spi, write to ram, addr increment
+					// when writing: (addr=0), read from ram, write spi, addr increment
+					// hence + dir_tospi for correction
+					oaddr <= addrbase + (512 - bytectr) + dir_tospi; 
+					//oaddr <= oaddr + 1'b1;
 					bytectr <= bytectr - 1'b1;
 					
 					if (0 == bytectr - 1'b1) 
