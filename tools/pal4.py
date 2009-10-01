17f5d80d849a6373cd7eb0e07b9c5509f9e20cd0
svofski <svofski@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2009-10-01 20:25:05 +0000

    pal calculator..

--- tools/pal4.py
+++ tools/pal4.py
@@ -6,11 +6,11 @@ def pal_coeffs(chord):
     
     print "\nU+V coefficients at R,G,B for %d phase angles (alpha-5/8):" % chord
 
-    for c in iter(map(lambda a:map(sum, zip(map(lambda x:x*cos(a),V),map(lambda x:x*sin(a),U))),  map(lambda x:(x-5/8)*2*pi/chord,range(chord)))):
+    for c in iter(map(lambda a:map(sum, zip(map(lambda x:x*cos(a),V),map(lambda x:x*sin(a),U))),  map(lambda x:(x-5/(2.0*chord))*2*pi/chord,range(chord)))):
         print "%+2.4f %+2.4f %+2.4f   uvsum #(%+4d,%+4d,%+4d)" % tuple(c+map(lambda x:int(x*(2*0.714*256/3.3)*0.72),c))
 	
     print "\nU-V coefficients at R,G,B for %d phase angles (alpha-3/8):" % chord
-    for c in iter(map(lambda a:map(sum, zip(map(lambda x:-x*cos(a),V),map(lambda x:x*sin(a),U))),  map(lambda x:(x-3/8)*2*pi/chord,range(chord)))):
+    for c in iter(map(lambda a:map(sum, zip(map(lambda x:-x*cos(a),V),map(lambda x:x*sin(a),U))),  map(lambda x:(x-3/(2.0*chord))*2*pi/chord,range(chord)))):
         print "%+2.4f %+2.4f %+2.4f   uvsum #(%+4d,%+4d,%+4d)" % tuple(c+map(lambda x:int(x*(2*0.714*256/3.3)*0.72),c))
 
 
@@ -26,20 +26,23 @@ def pal_calculate(clk,subcarrier,anglesteps,accu_bits):
 
 
 #pal_calculate(24,4.43361875,4,32)
-pal_calculate(24,4.43361875,8,32)
+pal_calculate(24,4.43361875,4,32)
 
 
 Fmclk=300e6
 Fsc=4433618.750
 width=32
 
-dPhase=8*Fsc/Fmclk*2**width
+dPhase=4*Fsc/Fmclk*2**width
 
 print "Fmclk=%f" % Fmclk
 print "Fsc=%f" % Fsc
 print "Phase accumulator width = %d" % width
 print "delta phase=%d" % int(round(dPhase))
-print "expected 8*Fsc=%f, Fsc=%f" % (dPhase*Fmclk/2**width, dPhase*Fmclk/2**width/8)
+print "expected 4*Fsc=%f, Fsc=%f" % (dPhase*Fmclk/2**width, dPhase*Fmclk/2**width/4)
+
+dPhase = dPhase - 60
+print "expected 4*Fsc=%f, Fsc=%f" % (dPhase*Fmclk/2**width, dPhase*Fmclk/2**width/4)
 
 Fsc=14000000
 width=16
