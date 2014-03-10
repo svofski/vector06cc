2c55027e0c3037e8f8bb1878d62cabbc677eae2d
ivagorRetrocomp@gmail.com <ivagorRetrocomp@gmail.com@f04d6dc0-7c41-0410-8f58-93aa6e29544b>
2014-03-10 16:41:12 +0000

    Good warp (almost 6 MHz) now, SDRAM works at 60 MHz

--- branches/sdram/src/clockster.v
+++ branches/sdram/src/clockster.v
@@ -21,7 +21,7 @@
 
 `default_nettype none
 
-module clockster(clk, clk50, clk24, clk18, ce12, ce6, ce6x, ce3, video_slice, pipe_ab, ce1m5, clkpalFSC);
+module clockster(clk, clk50, clk24, clk18, ce12, ce6, ce6x, ce3, video_slice, pipe_ab, ce1m5, clkpalFSC,clk60);
 input  [1:0] 	clk;
 input			clk50;
 output clk24;
@@ -34,6 +34,7 @@ output video_slice = qvideo_slice;
 output pipe_ab = qpipe_ab;
 output ce1m5 = qce1m5;
 output clkpalFSC;
+output clk60;
 
 reg[5:0] ctr;
 reg[4:0] initctr;
@@ -41,18 +42,11 @@ reg[4:0] initctr;
 reg qce12, qce6, qce6x, qce3, qce3v, qvideo_slice, qpipe_ab, qce1m5;
 
 wire lock;
-wire clk13_93;
-wire clk14_00;
-wire clk14_xx;
 
-wire clk300x;
 wire clk300;
-wire clk70k9;
+wire clk100;
 
-wire clk30;
-wire clk28;
-
-mclk24mhz vector_xtal(clk50, clk24, clk300, clk28, lock);
+mclk24mhz vector_xtal(clk50, clk24, clk300, clk60, lock);
 
 // Derive clock for PAL subcarrier: 4x 4.43361875
 `define PHACC_WIDTH 32
@@ -76,6 +70,8 @@ wire [30:0] cophacc_next = cophacc + `COPHACC_DELTA;
 always @(posedge clk300) cophacc <= cophacc_next;
 assign clk18 = cophacc[30];
 
+reg[15:0] clo=1'b1;
+reg clk4;
 always @(posedge clk24) begin
 	if (initctr != 3) begin
 		initctr <= initctr + 1'b1;
@@ -87,10 +83,12 @@ always @(posedge clk24) begin
 		qce6x <= ctr[1] & ~ctr[0];          // pre-pixel push @6mhz
 
 		qce3 <= ctr[2] & ctr[1] & !ctr[0]; //00100000 - svofski
-		
+
 		qvideo_slice <= !ctr[2];
-		qce1m5 <= !ctr[3] & ctr[2] & ctr[1] & !ctr[0]; 
+		qce1m5 <= !ctr[3] & ctr[2] & ctr[1] & !ctr[0];
 		ctr <= ctr + 1'b1;
+		clo<={clo[6:0],clo[7]};
+		clk4<=clo[0];
 	end
 end
 endmodule
